7214723133cd6ca66a1972ef172c2ae7d88a5fbf
ajay jha <ajay_jha@intuit.com>
2019-05-10 17:27:24 +0530

    assignment pushed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FavouriteScreen from './container/favouriteScreenContainer';
+import FoodList from './container/foodListContainer';
+import Header from './container/headerContainer';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import reducer from './redux/reducer';
+import thunk from 'redux-thunk';
+const store = createStore(reducer,applyMiddleware(thunk))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store} >
+      <div className="App">
+        <Header />
+        <FavouriteScreen />
+        <FoodList />
+      </div>
+    </Provider>
   );
 }
 
